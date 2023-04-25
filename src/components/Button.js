b47683e90342ffba0af2acc1ec2adcf06eab31b5
Ikenna Richard <oguejioforrichard@gmail.com>
2023-04-24 22:58:16 -1100

    Add button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React from 'react';
 
-export default function DigitButton({value}) {
+// eslint-disable-next-line react/prop-types
+export default function Button({ value }) {
   return (
-    <button type='button'>{value}</button>
-  )
+    <button type="button">{value}</button>
+  );
 }
