44c2f5d932d7d71bb4d76d2c122514ac379c9b45
Ikenna Richard <oguejioforrichard@gmail.com>
2023-04-25 00:59:23 -1100

    Setup button props

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -9,22 +9,22 @@ export default function Calculator() {
       <Button value="AC" />
       <Button value="+/-" />
       <Button value="%" />
-      <Button value="/" />
+      <Button value="÷" bg="orange" />
       <Button value={7} />
       <Button value={8} />
       <Button value={9} />
-      <Button value="*" />
+      <Button value="*" bg="orange" />
       <Button value={4} />
       <Button value={5} />
       <Button value={6} />
-      <Button value="-" />
+      <Button value="-" bg="orange" />
       <Button value={1} />
       <Button value={2} />
       <Button value={3} />
-      <Button value="+" />
-      <Button value={0} equals="equals" />
+      <Button value="+" bg="orange" />
+      <Button value={0} span="span-two" />
       <Button value="." />
-      <Button value="=" />
+      <Button value="=" bg="orange" />
     </div>
   );
 }
