8316ffbcf38909c83efca48fe214cb17584202b0
Ikenna Richard <oguejioforrichard@gmail.com>
2023-04-26 09:57:54 -1100

    Add quote component to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Calculator from './components/Calculator';
+import DisplayQuote from './components/DisplayQuote';
 
 function App() {
   return (
     <div className="App">
-      <h1>Math Magicians</h1>
       <Calculator />
+      <DisplayQuote />
     </div>
   );
 }
