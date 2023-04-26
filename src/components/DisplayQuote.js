9ab061915265d18932ecca8789bfa37020ec30e2
Ikenna Richard <oguejioforrichard@gmail.com>
2023-04-26 09:25:22 -1100

    Add function to fetch api data

---
+++ src/components/DisplayQuote.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+
+export default function DisplayQuote() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function getQuote() {
+      try {
+        const response = await fetch('https://api.api-ninjas.com/v1/quotes?category=computers', {
+          method: 'GET',
+          headers: { 'X-Api-Key': 'a4jkLzxa11Fyl351KUe2lQ==OqEqrHKzRyti4wax' },
+          contentType: 'application/json',
+        });
+        const result = await response.json();
+        setData(result);
+      } catch (e) {
+        throw new Error(e);
+      }
+    }
+    getQuote();
+  }, [setData]);
+
+  return (
+    <div>DisplayQuote</div>
+  );
+}
