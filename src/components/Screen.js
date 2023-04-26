f263311974bf02be09a424723032db659df295ba
Ikenna Richard <oguejioforrichard@gmail.com>
2023-04-25 16:48:32 -1100

    Update screen results

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,10 +1,14 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
-export default function Screen() {
+export default function Screen({ value }) {
+  const { total, next, operation } = value;
   return (
     <div className="output">
       <div className="current-operand">
-        0
+        {total}
+        {operation}
+        {next}
       </div>
     </div>
   );
