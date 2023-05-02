870f309d421efbbd42551f9cec95080227a5729b
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-01 23:48:46 -1100

    Add routes for the components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import DisplayQuote from './components/DisplayQuote';
 import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <Header />
       <div className="App">
         <Routes>
-          <Route path="/" element={<Calculator />} />
+          <Route exact path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
           <Route path="/quote" element={<DisplayQuote />} />
         </Routes>
       </div>
