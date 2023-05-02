a597eb4b13afa1cc835caef2ef074db6d1ece270
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-01 21:23:36 -1100

    Add header component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+
+export default function Header() {
+  return (
+    <header>
+        <h1>Math Magicians</h1>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li>Calculator</li>
+          <li>Quote</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
